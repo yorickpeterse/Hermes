b9ffdc1c985ecf469c61f934b5757801e7110d95
Yorick Peterse <yorickpeterse@gmail.com>
2012-07-30 20:49:14 +0200

    Strip newlines from the UD plugin and show a link.
    Signed-off-by: Yorick Peterse <yorickpeterse@gmail.com>

--- lib/hermes/plugin/urban_dictionary.rb
+++ lib/hermes/plugin/urban_dictionary.rb
@@ -43,10 +43,27 @@ module Hermes
           json = JSON.load(response.body)
 
           if json['list'][0]
-            item = json['list'][0]
+            item       = json['list'][0]
+            definition = item['definition']
+            example    = item['example']
+
+            if definition.length > 100
+              definition = definition[0..97] + '...'
+            end
+
+            if example.length > 100
+              example = example[0..97] + '...'
+            end
+
+            definition = definition.gsub(/\r\n|\n/, ' ').strip
+            example    = example.gsub(/\r\n|\n/, ' ').strip
 
             message.reply(
-              "#{item['definition']} | Example: #{item['example']}",
+              '%s | Example: %s | Link: %s' % [
+                definition,
+                example,
+                item['permalink']
+              ],
               true
             )
           else
