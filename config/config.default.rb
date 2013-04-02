fb0f1d72ac324501a18471842dfdc08aa43a2885
Yorick Peterse <yorickpeterse@gmail.com>
2013-04-02 20:41:59 +0200

    Dependencies update and Twitter configuration.
    Signed-off-by: Yorick Peterse <yorickpeterse@gmail.com>

--- config/config.default.rb
+++ config/config.default.rb
@@ -11,4 +11,11 @@ Hermes.bot.configure do |c|
   c.plugins.options[Hermes::Plugin::Weather] = {:key => ''}
 end
 
+Twitter.configure do |config|
+  config.consumer_key       = ''
+  config.consumer_secret    = ''
+  config.oauth_token        = ''
+  config.oauth_token_secret = ''
+end
+
 Hermes.bot.loggers.level = :info
