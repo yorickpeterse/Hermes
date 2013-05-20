d1f14b1f68489884969460a80d8ad979d24d7524
Yorick Peterse <yorickpeterse@gmail.com>
2013-05-20 18:27:24 +0200

    Add the status of eval.in to the output.
    Signed-off-by: Yorick Peterse <yorickpeterse@gmail.com>

--- lib/hermes/plugin/eval.rb
+++ lib/hermes/plugin/eval.rb
@@ -67,7 +67,7 @@ module Hermes
           response, json = Hermes.http.get_json(url + '.json')
           output         = json['output'].split("\n")[0]
 
-          message.reply("=> #{output} | #{url}", true)
+          message.reply("=> #{output} | #{json['status']} | #{url}", true)
         else
           message.reply(
             "The eval.in server shat itself. HTTP status: #{response.code}",
